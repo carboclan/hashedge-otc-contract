863098ee99604127810136efa417455fd1cb1774
Tal Rasha <talrasha007@gmail.com>
2019-03-18 23:21:12 +0800

    oracle compute profit.

--- contracts/tokens/Oracle.sol
+++ contracts/tokens/Oracle.sol
@@ -20,4 +20,24 @@ contract Oracle is WhitelistedRole {
 
     _oracles.push(OracleData(_ts, _profit));
   }
+
+  function computeProfit(uint64 start, uint64 end) public view returns(uint256 profit) {
+    require(_oracles.length > 0);
+    require(start >= _oracles[0].ts);
+
+    if (_oracles.length == 1) return _oracles[0].profitPerDayPerUnit;
+
+    for (uint256 d = _oracles.length - 1; d >= 0; d--) {
+      OracleData memory od = _oracles[d];
+      if (od.ts > end) continue;
+
+      if (start == od.ts || d == 0) {
+        profit += od.profitPerDayPerUnit * (end - start) / 24 / 3600;
+        break;
+      } else {
+        profit += od.profitPerDayPerUnit * (end - od.ts) / 24 / 3600;
+        end = od.ts;
+      }
+    }
+  }
 }
