9e6738cc928c6450c690859361ed9671bc459d77
Tal Rasha <talrasha007@gmail.com>
2019-03-18 22:12:48 +0800

    Collateral basic.

---
+++ contracts/tokens/Collateral.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+contract Collateral {
+  IERC20 _underlying;
+
+  struct info {
+    uint256 balance;
+    uint256 margin;
+  }
+
+  mapping(address => info) _accountInfo;
+
+  constructor(address erc20Addr) public {
+    _underlying = ERC20(erc20Addr);
+  }
+
+  function deposit(uint256 value) public {
+    _underlying.transferFrom(msg.sender, address(this), value);
+    _accountInfo[msg.sender].balance += value;
+  }
+
+  function withdraw(uint256 value) public {
+    info storage inf = _accountInfo[msg.sender];
+    require(value <= inf.balance - inf.margin);
+
+    info.balance -= value;
+    _underlying.transfer(msg.sender, value);
+  }
+}
