1fb175a2e0fdd398716b73b70266e8aa72bd4593
Tal Rasha <talrasha007@gmail.com>
2019-03-30 13:45:16 +0800

    test deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,43 @@
-const BtcSwap721 = artifacts.require('./tokens/Swap721.sol');
+const ERC20 = artifacts.require('./tokens/TestERC20.sol');
+const Collateral = artifacts.require('./tokens/Collateral.sol');
+const Oracle = artifacts.require('./tokens/Oracle.sol');
+const Swap721 = artifacts.require('./tokens/Swap721.sol');
 
 module.exports = async function(deployer) {
   deployer.then(async () => {
-    // await deployer.deploy(BtcSwap721);
+    if (process.env.PROD !== true) {
+      await deployer.deploy(ERC20, 'FIX', 'FIX');
+      const fixLegToken = await ERC20.deployed();
+
+      await deployer.deploy(ERC20, 'FLOATING', 'FLT');
+      const floatingLegToken = await ERC20.deployed();
+
+      await deployer.deploy(Collateral, floatingLegToken.address);
+      const collateral = await Collateral.deployed();
+
+      await deployer.deploy(Oracle);
+      const oracle = await Oracle.deployed();
+
+      await deployer.deploy(Swap721, 'SWAP TEST', 'SWT', 'TH/s', 'PoW', oracle.address, fixLegToken.address, collateral.address);
+      const swap721 = await Swap721.deployed();
+
+      const output = {
+        erc20Tokens: {
+          [fixLegToken.address]: fixLegToken.abi,
+          [floatingLegToken.address]: floatingLegToken.abi
+        },
+        collaterals: {
+          [collateral.address]: collateral.abi
+        },
+        oracles: {
+          [oracle.address]: oracle.abi
+        },
+        swap721Tokens: {
+          [swap721.address]: swap721.abi
+        }
+      };
+
+      require('fs').writeFileSync(__dirname + '/../build/abi.json', JSON.stringify(output));
+    }
   });
 };
