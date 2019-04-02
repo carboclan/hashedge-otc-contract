947f96b442f971330a54e6a7c6cebba3f15d6e0b
Tal Rasha <talrasha007@gmail.com>
2019-04-03 00:30:01 +0800

    update dev deploy for test.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,36 +6,72 @@ const Swap721 = artifacts.require('./tokens/Swap721.sol');
 module.exports = async function(deployer) {
   deployer.then(async () => {
     if (process.env.PROD !== true) {
-      await deployer.deploy(ERC20, 'FIX', 'FIX');
-      const fixLegToken = await ERC20.deployed();
+      await deployer.deploy(ERC20, 'DAI', 'DAI');
+      const dai = await ERC20.deployed();
 
-      await deployer.deploy(ERC20, 'FLOATING', 'FLT');
-      const floatingLegToken = await ERC20.deployed();
+      await deployer.deploy(ERC20, 'WBTC', 'WBTC');
+      const wbtc = await ERC20.deployed();
 
-      await deployer.deploy(Collateral, floatingLegToken.address);
-      const collateral = await Collateral.deployed();
+      await deployer.deploy(ERC20, 'WETH', 'WETH');
+      const weth = await ERC20.deployed();
+
+      await deployer.deploy(Collateral, wbtc.address);
+      const btcCollateral = await Collateral.deployed();
+
+      await deployer.deploy(Collateral, weth.address);
+      const ethCollateral = await Collateral.deployed();
 
       await deployer.deploy(Oracle);
       const oracle = await Oracle.deployed();
 
-      await deployer.deploy(Swap721, 'SWAP TEST', 'SWT', 'TH/s', 'PoW', oracle.address, fixLegToken.address, collateral.address);
-      const swap721 = await Swap721.deployed();
+      await deployer.deploy(Swap721, 'BTC-POW', 'BTC-POW', 'TH/s', 'PoW', oracle.address, dai.address, btcCollateral.address);
+      const btcSwap721 = await Swap721.deployed();
 
-      await collateral.addWhitelisted(swap721.address);
+      await deployer.deploy(Swap721, 'ETH-POW', 'ETH-POW', 'GH/s', 'PoW', oracle.address, dai.address, ethCollateral.address);
+      const ethSwap721 = await Swap721.deployed();
+
+      await btcCollateral.addWhitelisted(btcSwap721.address);
+      await ethCollateral.addWhitelisted(ethSwap721.address);
+
+      await oracle.addWhitelisted('0xde8fc290a265c78d4cfd1c509241dae22687193e');
+      await btcSwap721.addWhitelisted('0xde8fc290a265c78d4cfd1c509241dae22687193e');
+      await ethSwap721.addWhitelisted('0xde8fc290a265c78d4cfd1c509241dae22687193e');
+
+      await dai.mint('0xde8fc290a265c78d4cfd1c509241dae22687193e', 1e19.toString());
+      await wbtc.mint('0xde8fc290a265c78d4cfd1c509241dae22687193e', 1e19.toString());
+      await weth.mint('0xde8fc290a265c78d4cfd1c509241dae22687193e', 1e19.toString());
+
+      await oracle.appendOracleData(Math.round(Date.now() / 1000 - 3600 * 24), 1e18.toString());
+      await oracle.appendOracleData(Math.round(Date.now() / 1000 - 3600), 1e18.toString());
+
+      await wbtc.approve(btcCollateral.address, 2e18.toString());
+      await btcCollateral.deposit(2e18.toString());
+      await btcSwap721.mint(24 * 3600 / 27, 27, 1e18.toString(), 2);
+
+      await dai.approve(btcSwap721.address, 1e18.toString());
+      try { await btcSwap721.initialBuy([0]); }
+      catch (e) { }
+
+      await weth.approve(ethCollateral.address, 2e18.toString());
+      await ethCollateral.deposit(2e18.toString());
+      await ethSwap721.mint(24 * 3600 / 27, 27, 1e18.toString(), 2);
 
       const output = {
         erc20Tokens: {
-          [fixLegToken.address.toLowerCase()]: fixLegToken.abi,
-          [floatingLegToken.address.toLowerCase()]: floatingLegToken.abi
+          [dai.address.toLowerCase()]: dai.abi,
+          [wbtc.address.toLowerCase()]: wbtc.abi,
+          [weth.address.toLowerCase()]: weth.abi
         },
         collaterals: {
-          [collateral.address.toLowerCase()]: collateral.abi
+          [btcCollateral.address.toLowerCase()]: btcCollateral.abi,
+          [ethCollateral.address.toLowerCase()]: ethCollateral.abi
         },
         oracles: {
           [oracle.address.toLowerCase()]: oracle.abi
         },
         swap721Tokens: {
-          [swap721.address.toLowerCase()]: swap721.abi
+          [btcSwap721.address.toLowerCase()]: btcSwap721.abi,
+          [ethSwap721.address.toLowerCase()]: ethSwap721.abi
         }
       };
 
