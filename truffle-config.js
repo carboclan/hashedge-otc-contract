603e1305e87f69b03b962160daf41e03c186eb62
Tal Rasha <talrasha007@gmail.com>
2019-04-10 17:27:47 +0800

    v3 infura

--- truffle-config.js
+++ truffle-config.js
@@ -19,7 +19,7 @@
  */
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = '8Q30zi3TAqlJ6JSCwcul';
+const infuraKey = '06fd041b5d42478cb9d9b68b35069d85';
 
 module.exports = {
   /**
@@ -58,7 +58,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(process.env.PRI, `https://ropsten.infura.io/${infuraKey}`),
+      provider: () => new HDWalletProvider(process.env.PRI, `https://ropsten.infura.io/v3/${infuraKey}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
